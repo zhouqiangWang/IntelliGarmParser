acf5abdfa55190ea7cce0223653db7e6d192761d
zhouqiang.wzq <zhouq.wang@gmail.com>
2018-10-11 16:47:27 -0700

    add Oct11 archecture.

--- parser.js
+++ parser.js
@@ -1,6 +1,6 @@
 const PDFParser = require('pdf2json');
 
-let pdfParser = new PDFParser();
+let pdfParser = new PDFParser(this, 1);
 
 pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
 pdfParser.on("pdfParser_dataReady", pdfData => {
@@ -16,8 +16,12 @@ pdfParser.on("pdfParser_dataReady", pdfData => {
     			// console.log(decodeURIComponent(textArr[j].R[0].T));
     		}
     	}
+    	// console.log(texts);
 
-    	console.log(texts);
+    	let data = pdfParser.getRawTextContent();
+    	console.log("text = ", data);
 	});
 
-pdfParser.loadPDF("/Users/mac/Downloads/ET17-12-052_W\ JULIETTE\ MULTI\ STRIPE\ DRESS/bulk/BOM/W\ Juliette\ Multi\ Stripe\ Dress\ -\ 3.1\ updated\ gws\ Candy.pdf");
+// pdfParser.loadPDF("/Users/mac/Downloads/ET17-12-052_W\ JULIETTE\ MULTI\ STRIPE\ DRESS/bulk/BOM/W\ Juliette\ Multi\ Stripe\ Dress\ -\ 3.1\ updated\ gws\ Candy.pdf");
+// pdfParser.loadPDF("/Users/mac/Downloads/ET17-12-052_W\ JULIETTE\ MULTI\ STRIPE\ DRESS/bulk/FIT\ CMNTS\ APP_D/W\ Juliette\ Multi\ Stripe\ Dress\ -\ 12.27\ proto\ fit\ approved.pdf");
+pdfParser.loadPDF("/Users/mac/Downloads/ET17-12-052_W\ JULIETTE\ MULTI\ STRIPE\ DRESS/development/0921/BOM-\ W\ JULIETTE\ MULTI\ STRIPE\ DRESS\ 9.21.pdf");
